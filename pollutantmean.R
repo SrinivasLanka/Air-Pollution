1d1674bc52a47eeb57f89ab3c7da6490596c6178
SrinivasLanka <vaasulanka@gmail.com>
2015-10-18 19:33:08 +0530

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,12 +1,23 @@
+## This function will return the mean of the specified pollutant
+## across all monitors (ignoring NA values)
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  data <- data.frame();
-  files <- list.files(directory, full.names = TRUE);
+  # create character vector of filenames based on passed id
+  datafiles <- c()
+  datafiles <- c(datafiles, paste(directory, "/", formatC(id, width=3, flag="0"), ".csv", sep=""))
   
-  for (index in files) {
-    data <- rbind(data, read.csv(index, comment.char = ""))
-  }
+  # read all files specified above
+  my_data <- lapply(datafiles, read.csv)
+  my_data <- do.call(rbind, my_data)
   
-  neededMonitors <- subset(data, ID %in% id);
-  pollutantMean <- mean(neededMonitors[[pollutant]], na.rm = TRUE);
-  pollutantMean;
+  #display sulfate or nitrate mean
+  if (pollutant == colnames(my_data)[2]) {
+    sulfate <- mean(my_data[,2], na.rm = TRUE)
+    sulfate <- round(sulfate, digits = 3)
+    return(sulfate)
+  } else {
+    nitrate <- mean(my_data[,3], na.rm = TRUE)
+    nitrate <- round(nitrate, digits = 3)
+    return(nitrate) 
+  }       
 }
